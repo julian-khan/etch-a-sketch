fc24014e5c8567bc0e358ec1a1551b9995e6eb96
Julian Khan <juliankhan.b@gmail.com>
2022-08-15 18:50:17 +0800

    Fix syntax error in createGrid

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 function createGrid(cellCount=64) { 
     const gridWrapper = document.querySelector('.grid-wrapper');
 
-    for (let i = 1; i <= cellCount, i++) { 
+    for (let i = 1; i <= cellCount; i++) { 
         gridWrapper.appendChild(createDivCell(i));
+    }
 }
+
 
 function createDivCell(cellNum) {  //returns a div
 
-    const gridRow = document.createElement('div');
-    gridRow.textContent = `${i}`;
-    return gridRow;
+    const cell = document.createElement('div');
+    cell.textContent = `${cellNum}`;
+    return cell;
 }
+
+console.log(typeof createDivCell(1));
