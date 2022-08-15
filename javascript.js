ac25477902b0424a70be3106cd427273e4e117bd
Julian Khan <juliankhan.b@gmail.com>
2022-08-15 18:45:22 +0800

    Change createRow name to createDivCell

--- javascript.js
+++ javascript.js
@@ -2,10 +2,10 @@ function createGrid(dimension) {
     const gridWrapper = document.querySelector('.grid-wrapper');
 
     for (let i = 1; i <= dimension, i++) { 
-        createRow(dimension);
+        gridWrapper.appendChild(createDivCell(dimension));
 }
 
-    function createRow(dimension) {  //returns a div
+function createDivCell(dimension) {  //returns a div
 
     const gridRow = document.createElement('div');
     gridRow.textContent = `Row ${i}`;
